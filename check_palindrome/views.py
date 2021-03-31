d1984d6cdc74aaf3b9a83cac2422b412b1c26570
AjmalPonneth <ponnethajmal@gmail.com>
2021-03-31 14:44:46 +0530

    added palindrome function

--- check_palindrome/views.py
+++ check_palindrome/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from .forms import PalindromeForm
 # Create your views here.
 
 
 def check_palindrome_or_not(request):
-    return render(request, 'index.html')
+    form = PalindromeForm()
+    context = {
+        'form': form
+    }
+    if form.is_valid:
+        palindrome = request.POST.get('palindrome')
+        IsPalindrome = palindrome.find(palindrome[::-1]) == 0
+        print(IsPalindrome)
+    else:
+        PalindromeForm()
+    return render(request, 'index.html', context)
